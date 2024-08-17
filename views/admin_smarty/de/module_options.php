1e63bc46173c2f082ab38481ddae65ccf15dddeb
Ivo Bathke <ivo.bathke@gmail.com>
2024-08-17 15:38:32 +0200

    translations

--- views/admin_smarty/de/module_options.php
+++ views/admin_smarty/de/module_options.php
@@ -4,6 +4,6 @@ $sLangName = 'Deutsch';
 
 $aLang = [
     'charset' => 'UTF-8',
-    'SHOP_MODULE_GROUP_main' => 'Einstellungen',
-    'SHOP_MODULE_token' => 'Trenner',
+    'SHOP_MODULE_GROUP_ivoba_split_category_desc_main' => 'Einstellungen',
+    'SHOP_MODULE_ivoba_split_category_desc_token' => 'Trenner',
 ];
--- views/admin_smarty/en/module_options.php
+++ views/admin_smarty/en/module_options.php
@@ -4,6 +4,6 @@ $sLangName = 'Deutsch';
 
 $aLang = [
     'charset' => 'UTF-8',
-    'SHOP_MODULE_GROUP_main' => 'Settings',
-    'SHOP_MODULE_token' => 'Token',
+    'SHOP_MODULE_GROUP_ivoba_split_category_desc_main' => 'Settings',
+    'SHOP_MODULE_ivoba_split_category_desc_token' => 'Token',
 ];
